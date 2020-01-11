ad561db783d591877ed8c8660a8f905294a82ed3
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2020-01-11 17:44:36 +0400

    Updates

---
+++ precache-manifest.af281f95d4ffcdd4cc01574c41e666cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4354c827eec2cc02cd8f1f2341cee379",
+    "url": "/exp/index.html"
+  },
+  {
+    "revision": "3227f38db50de220dec0",
+    "url": "/exp/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "3227f38db50de220dec0",
+    "url": "/exp/static/js/2.d4249eb6.chunk.js"
+  },
+  {
+    "revision": "cc1e14afe3a23d235390",
+    "url": "/exp/static/js/main.38a86773.chunk.js"
+  },
+  {
+    "revision": "41250fcd24ea6a914c5e",
+    "url": "/exp/static/js/runtime-main.b8c63a5a.js"
+  },
+  {
+    "revision": "49e2f0ce226c4a43ffff80591ee3b075",
+    "url": "/exp/static/media/persik.49e2f0ce.png"
+  }
+]);
